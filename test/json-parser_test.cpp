9e2197444b037206f16d740f18966f7d12a93eb6
the-doctor-octo <glefe85@yahoo.it>
2024-04-10 06:34:30 +0200

    fix: fix some unit test

--- test/json-parser_test.cpp
+++ test/json-parser_test.cpp
@@ -7,24 +7,27 @@
 TEST(JsonParserTest, setContent_does_not_throw)
 {
     JSONParser *parser = new JSONParser();
-    EXPECT_NO_THROW(parser->setContent("test"));
+    std::string content = "test";
+    EXPECT_NO_THROW(parser->setContent(content));
 }
 
 TEST(JsonParserTest, getContent)
 {
     JSONParser *parser = new JSONParser();
-    parser->setContent("test");
+    std::string content = "test";
+    parser->setContent(content);
 
-    std::string content = parser->getContent();
+    std::string result = parser->getContent();
 
     // Expect equality.
-    EXPECT_EQ(content, "test");
+    EXPECT_EQ(result, "test");
 }
 
 TEST(JsonParserTest, parse_does_not_throw)
 {
     JSONParser *parser = new JSONParser();
-    parser->setContent("{\"propertyA\":\"test\"}");
+    std::string content = "{\"propertyA\":\"test\"}";
+    parser->setContent(content);
 
     EXPECT_NO_THROW(parser->parse());
 }
@@ -32,13 +35,14 @@ TEST(JsonParserTest, parse_does_not_throw)
 TEST(JsonParserTest, parse_then_getProperty)
 {
     JSONParser *parser = new JSONParser();
-    parser->setContent("{\"propertyA\":\"test\"}");
+    std::string content = "{\"propertyA\":\"test\"}";
+    parser->setContent(content);
 
     EXPECT_NO_THROW(parser->parse());
 
     std::cout << "\n reading propertyA \n";
-    std::string prop_value = parser->getProperty("propertyA");
+    JSONValue prop_value = parser->getProperty("propertyA");
 
-    std::cout << "\n propertyA: " << prop_value << "; \n";
-    EXPECT_EQ(prop_value, "test");
+    std::cout << "\n propertyA: " << prop_value.second << "; \n";
+    EXPECT_EQ(prop_value.second, "test");
 }
